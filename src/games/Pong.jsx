d57dc7ef2a85cc177ea99b44daf96eeeef3d9dc0
RinseWillet <r.willet@gmail.com>
2025-04-25 17:17:11 +0200

    feat: add react-router setup and create Pong route

---
+++ src/games/Pong.jsx
@@ -0,0 +1,10 @@
+const Pong = () => {
+    return (
+      <div className="min-h-screen bg-black text-pink-400 flex flex-col items-center justify-center">
+        <h1 className="text-5xl font-bold neon-text mb-6">Pong</h1>
+        <p>Retro Pong will go here!</p>
+      </div>
+    )
+  }
+  
+  export default Pong
